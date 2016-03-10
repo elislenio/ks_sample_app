d41440273d6f47b7bf4b4861b76dd548b3fc1541
Guido Donnari <gdonnari@yahoo.com.ar>
2016-03-10 19:53:49 -0300

    removed ODM
    removed ODM

--- app/autoload.php
+++ app/autoload.php
@@ -11,6 +11,5 @@ error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/phpcr-odm/lib/Doctrine/ODM/PHPCR/Mapping/Annotations/DoctrineAnnotations.php');
 
 return $loader;
